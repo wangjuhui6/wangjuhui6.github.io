8b90cc1fc305e48f453cb35c11dab406a0f4d869
wangjuhui <13934720500@163.com>
2023-05-11 17:16:55 +0800

    feat: tensorflow部署

---
+++ tensorflow/assets/HomeView-2bbab541.js
@@ -0,0 +1,1 @@
+import{d as r,c as t,F as s,r as o,u as n,o as a,a as _,w as u,R as c,b as l,e as d,t as p}from"./index-5bd7dae7.js";const g=r({__name:"HomeView",setup(i){return(m,f)=>(a(),t("ul",null,[(a(!0),t(s,null,o(n(l).getRoutes(),e=>(a(),t("li",{key:e.name},[_(n(c),{to:e.path},{default:u(()=>[d(p(e.meta.name),1)]),_:2},1032,["to"])]))),128))]))}});export{g as default};
